cce45a5aef27585853e9ab3bdcdd32a2dbd1b839
Antoni <antoni-devlin@users.noreply.github.com>
2023-12-08 12:12:14 +0000

    Started day 2

--- day_2/cubes.py
+++ day_2/cubes.py
@@ -1,4 +1,24 @@
+import fileinput
+import re
+
+test_string = "Game 1: 19 blue, 12 red; 19 blue, 2 green, 1 red; 13 red, 11 blue"
+
 cubes = {"red": 12, "green": 13, "blue": 14}
 
-for line in fileinput.input():
-    print(line)
+
+def get_game_info(gameString):
+    gameID = re.findall(r"^Game\s\d+", gameString)[0].split(" ")[1]
+    redCubes = [sub.replace(" red", "") for sub in re.findall(r"\d+\sred", gameString)]
+    greenCubes = [
+        sub.replace(" green", "") for sub in re.findall(r"\d+\sgreen", gameString)
+    ]
+    blueCubes = [
+        sub.replace(" blue", "") for sub in re.findall(r"\d+\sblue", gameString)
+    ]
+    return gameID, redCubes, greenCubes, blueCubes
+
+
+print(get_game_info(test_string))
+
+# for line in fileinput.input():
+#     print(line)
